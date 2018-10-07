be46f0560c4b221abdcfcec0e996d3f4110548d8
lixiang <348816986@qq.com>
2018-10-07 11:48:49 +0800

    修复创建用户没有部门的bug

--- src/components/Users/UserModal.js
+++ src/components/Users/UserModal.js
@@ -203,7 +203,7 @@ class UserEditModal extends Component {
               pass = true;
               values.group = parent[3];
               values.team = parent[2];
-              values.director = parent[1];
+              values.department = parent[1];
               values.unit = parent[0];
             }
             break;
